e3171678288071c82f7de6bc44d5678322ae0264
tang1125@uw.edu <tang1125@uw.edu>
2024-02-19 09:04:14 -0800

    2/19

--- mazes/src/disjointsets/UnionBySizeCompressingDisjointSets.java
+++ mazes/src/disjointsets/UnionBySizeCompressingDisjointSets.java
@@ -1,5 +1,6 @@
 package disjointsets;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,6 +10,8 @@ import java.util.List;
 public class UnionBySizeCompressingDisjointSets<T> implements DisjointSets<T> {
     // Do NOT rename or delete this field. We will be inspecting it directly in our private tests.
     List<Integer> pointers;
+    List<T> contents;
+    int size;
 
     /*
     However, feel free to add more fields and private helper methods. You will probably need to
@@ -17,24 +20,41 @@ public class UnionBySizeCompressingDisjointSets<T> implements DisjointSets<T> {
 
     public UnionBySizeCompressingDisjointSets() {
         // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        this.pointers = new ArrayList<>();
+        this.contents =  new ArrayList<>();
+        this.size = 0;
     }
 
     @Override
     public void makeSet(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        this.pointers.add(-1);
+        this.contents.add(item);
     }
 
     @Override
     public int findSet(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        int index = this.contents.indexOf(item);
+        if (index == -1) {
+            return -1;
+        }
+        if (this.pointers.get(index) != -1){
+            this.pointers.set(index, findSet(contents.get(pointers.get(index))));
+        }
+
+        return pointers.get(index) == -1 ? index : pointers.get(index);
+
     }
 
     @Override
     public boolean union(T item1, T item2) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        int index1 = findSet(item1);
+        int index2 = findSet(item2);
+        if (index1 == -1 || index2 == -1 || index1 == index2) {
+            return false;
+        }
+        //same weight situation ?
+        //integer id?
+
+        return true;
     }
 }
