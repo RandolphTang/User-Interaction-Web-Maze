c02f021425f8dc28723d0b01be9e2002f6e10168
tang1125@uw.edu <tang1125@uw.edu>
2024-02-21 09:11:37 -0800

    2/20

--- mazes/src/graphs/shortestpaths/DijkstraShortestPathFinder.java
+++ mazes/src/graphs/shortestpaths/DijkstraShortestPathFinder.java
@@ -5,7 +5,13 @@ import graphs.Graph;
 import priorityqueues.DoubleMapMinPQ;
 import priorityqueues.ExtrinsicMinPQ;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Computes shortest paths using Dijkstra's algorithm.
@@ -31,14 +37,63 @@ public class DijkstraShortestPathFinder<G extends Graph<V, E>, V, E extends Base
 
     @Override
     protected Map<V, E> constructShortestPathsTree(G graph, V start, V end) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+
+        HashMap<V, E> spt = new HashMap<>();
+
+        ExtrinsicMinPQ<V> minPQ = createMinPQ();
+
+        HashMap<V, Double> distance = new HashMap<>();
+
+        if (Objects.equals(start, end)) {
+            return spt;
+        }
+
+        minPQ.add(start, 0.0);
+        distance.put(start, 0.0);
+
+        while (!minPQ.isEmpty()) {
+            V curr = minPQ.removeMin();
+            if (curr.equals(end)) {
+                break;
+            }
+
+            for (E edge : graph.outgoingEdgesFrom(curr)) {
+                V next = edge.to();
+                double newDistance = distance.get(curr) + edge.weight();
+
+                if (!distance.containsKey(next) || newDistance < distance.get(next)) {
+                    distance.put(next, newDistance);
+                    spt.put(next, edge);
+                    minPQ.add(next, newDistance);
+                }
+            }
+        }
+
+        return spt;
     }
 
     @Override
     protected ShortestPath<V, E> extractShortestPath(Map<V, E> spt, V start, V end) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+
+        List<E> collection = new ArrayList<>();
+
+        if (!spt.containsKey(end)) {
+            return new ShortestPath.Failure<>();
+        }
+
+        V finalL = end;
+        while (finalL != null && !finalL.equals(start)) {
+            E edge = spt.get(finalL);
+            collection.add(edge);
+            finalL = edge.to();
+        }
+
+        if (!Objects.equals(finalL, start)) {
+            return new ShortestPath.Failure<>();
+        }
+
+        return new ShortestPath.Success<>(collection);
+
     }
 
 }
